2f046a6619e8ad1b063e57639ec60a53170ebc47
Deseteral <deseteral@gmail.com>
2023-06-03 10:11:28 +0200

    Speed up video processing by using multiple threads

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::fs;
 use std::process::Command;
 use std::time::SystemTime;
+use std::{fs, thread, vec};
 use tauri::Manager;
 
 static WORKING_DIR_PATH: &'static str = "/tmp/ohroniasz";
 
 // TODO: This could be multithreaded.
-fn concat_video_files(output_name: &str) {
+fn concat_video_files(src_list_path: &str, output_path: &str) {
     Command::new("ffmpeg")
         .arg("-y")
         .arg("-f")
@@ -17,10 +17,10 @@ fn concat_video_files(output_name: &str) {
         .arg("-safe")
         .arg("0")
         .arg("-i")
-        .arg(format!("{WORKING_DIR_PATH}/list.txt"))
+        .arg(src_list_path)
         .arg("-c")
         .arg("copy")
-        .arg(format!("{WORKING_DIR_PATH}/{output_name}.mp4"))
+        .arg(output_path)
         .output()
         .expect("ffmpeg failed");
 }
@@ -29,6 +29,7 @@ fn generate_preview_files_for_directory(directory_path: &str) {
     let time_start = SystemTime::now();
 
     let positions = ["front", "back", "left_repeater", "right_repeater"];
+    let mut workers = vec![];
 
     for position in positions {
         let mut files: Vec<String> = vec![];
@@ -52,8 +53,17 @@ fn generate_preview_files_for_directory(directory_path: &str) {
             .collect::<Vec<_>>()
             .join("\n");
 
-        fs::write(format!("{WORKING_DIR_PATH}/list.txt"), list_file_content).unwrap();
-        concat_video_files(&position);
+        let src_list_path = format!("{WORKING_DIR_PATH}/list-{position}.txt");
+        fs::write(&src_list_path, &list_file_content).unwrap();
+
+        workers.push(thread::spawn(move || {
+            let output_path = format!("{WORKING_DIR_PATH}/{position}.mp4");
+            concat_video_files(&src_list_path, &output_path);
+        }));
+    }
+
+    for worker in workers {
+        let _ = worker.join();
     }
 
     let time_end = SystemTime::now();
@@ -71,6 +81,10 @@ fn create_working_dir() {
 
 fn main() {
     create_working_dir();
+
+    generate_preview_files_for_directory(
+        "/Users/deseteral/Downloads/TeslaCam/SavedClips/2023-05-29_14-09-51",
+    );
 
     tauri::Builder::default()
         .setup(|app| {
