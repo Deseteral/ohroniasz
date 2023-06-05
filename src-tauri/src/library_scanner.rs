e83a1d62e3cc406e36db7d557cd5eed2ed9905a7
Deseteral <deseteral@gmail.com>
2023-06-06 00:43:13 +0200

    Simplify library scanner

--- src-tauri/src/library_scanner.rs
+++ src-tauri/src/library_scanner.rs
@@ -1,7 +1,7 @@
 use serde::Serialize;
 use serde_json::Value;
 use std::{
-    fs,
+    fs::{self, DirEntry},
     path::{Path, PathBuf},
     vec,
 };
@@ -37,51 +37,42 @@ pub fn scan_library(library_path: &Path) -> Vec<CamEvent> {
 
     for dir_entry in fs::read_dir(&saved_clips_path).unwrap() {
         let dir_entry = dir_entry.unwrap();
-        let path = dir_entry.path().clone();
-
-        if path.is_file() {
-            continue;
+        if let Some(cam_event) = dir_entry_to_cam_event(dir_entry, CamEventKind::SavedClip) {
+            events.push(cam_event);
         }
-
-        let dir_name = dir_entry.file_name().into_string().unwrap();
-
-        let metadata_str = fs::read_to_string(path.join("event.json")).unwrap();
-        let metadata: Value = serde_json::from_str(&metadata_str).unwrap();
-        let city = metadata["city"].as_str().unwrap().to_string();
-
-        events.push(CamEvent {
-            date: dir_name_to_date(&dir_name),
-            path,
-            kind: CamEventKind::SavedClip,
-            location: CamEventLocation { city },
-        });
     }
 
     for dir_entry in fs::read_dir(&sentry_clips_path).unwrap() {
         let dir_entry = dir_entry.unwrap();
-        let path = dir_entry.path().clone();
-
-        if path.is_file() {
-            continue;
+        if let Some(cam_event) = dir_entry_to_cam_event(dir_entry, CamEventKind::SentryClip) {
+            events.push(cam_event);
         }
-
-        let dir_name = dir_entry.file_name().into_string().unwrap();
-
-        let metadata_str = fs::read_to_string(path.join("event.json")).unwrap();
-        let metadata: Value = serde_json::from_str(&metadata_str).unwrap();
-        let city = metadata["city"].as_str().unwrap().to_string();
-
-        events.push(CamEvent {
-            date: dir_name_to_date(&dir_name),
-            path,
-            kind: CamEventKind::SentryClip,
-            location: CamEventLocation { city },
-        });
     }
 
     events.sort_by(|a, b| b.date.cmp(&a.date));
 
     events
+}
+
+fn dir_entry_to_cam_event(dir_entry: DirEntry, event_kind: CamEventKind) -> Option<CamEvent> {
+    let path = dir_entry.path().clone();
+
+    if path.is_file() {
+        return None;
+    }
+
+    let dir_name = dir_entry.file_name().into_string().unwrap();
+
+    let metadata_str = fs::read_to_string(path.join("event.json")).unwrap();
+    let metadata: Value = serde_json::from_str(&metadata_str).unwrap();
+    let city = metadata["city"].as_str().unwrap().to_string();
+
+    Some(CamEvent {
+        date: dir_name_to_date(&dir_name),
+        path,
+        kind: event_kind,
+        location: CamEventLocation { city },
+    })
 }
 
 fn dir_name_to_date(dir_name: &str) -> String {
