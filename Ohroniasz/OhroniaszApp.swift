f76f1526c3bb19c9a533bbc04611392a25a936e4
Deseteral <deseteral@gmail.com>
2023-06-08 20:44:34 +0200

    Transition to native SwiftUI

---
+++ Ohroniasz/OhroniaszApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct OhroniaszApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- src-tauri/src/config.rs
+++
@@ -1,7 +0,0 @@
-use std::fs;
-
-pub static WORKING_DIR_PATH: &str = "/tmp/ohroniasz";
-
-pub fn create_working_dir() {
-    fs::create_dir_all(WORKING_DIR_PATH).expect("Could not create app working directory");
-}
