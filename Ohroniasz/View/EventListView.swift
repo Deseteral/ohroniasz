7658f4f6e413f21259107fdca89e97ba584a27c8
Deseteral <deseteral@gmail.com>
2023-06-09 23:47:18 +0200

    Date styling

--- Ohroniasz/View/EventListView.swift
+++ Ohroniasz/View/EventListView.swift
@@ -3,11 +3,14 @@ import SwiftUI
 struct EventListView: View {
     let events: [CamEvent]
     
-    let dtFormatter = DateFormatter()
+    @State private var dateColumnWidth: CGFloat = 160
+    private let dateFormatter = DateFormatter()
     
     init(events: [CamEvent]) {
         self.events = events
-        dtFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.timeStyle = .medium
+        dateFormatter.dateStyle = .long
+        dateFormatter.locale = Locale.autoupdatingCurrent
     }
     
     var body: some View {
@@ -17,14 +20,14 @@ struct EventListView: View {
                 case .savedClip: Image(systemName: "externaldrive.fill").foregroundColor(.blue)
                 case .sentryClip: Image(systemName: "circle.fill").foregroundColor(.red)
                 }
-                
             }
             .width(15)
             
             TableColumn("Date") { event in
-                Text(dtFormatter.string(from: event.date))
+                Text(dateFormatter.string(from: event.date))
+                    .overlay( GeometryReader { geo in Color.clear.onAppear { self.dateColumnWidth = geo.size.width }})
             }
-            .width(160)
+            .width(self.dateColumnWidth)
 
             TableColumn("Path", value: \.path)
         }
