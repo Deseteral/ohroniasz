b4d497170e1b37d406d9472155e1202d5ea85a1d
Deseteral <deseteral@gmail.com>
2023-06-24 18:58:38 +0200

    Dummy favorites toolbar button

--- Ohroniasz/View/ToolbarContent.swift
+++ Ohroniasz/View/ToolbarContent.swift
@@ -4,6 +4,13 @@ struct ContentToolbar: ToolbarContent {
     var body: some ToolbarContent {
         ToolbarItemGroup {
             Button {
+                print("mark as favorie")
+            } label: {
+                Image(systemName: "star")
+            }
+            .help("Mark as favorite")
+
+            Button {
                 print("show in finder")
             } label: {
                 Image(systemName: "folder")
