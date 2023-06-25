cdb7e8dbdec08bf86fbdeaab7ba3ec108ab42a91
Deseteral <deseteral@gmail.com>
2023-06-25 19:57:23 +0200

    Correct label rendering inside sidebar

--- Ohroniasz/View/SidebarView.swift
+++ Ohroniasz/View/SidebarView.swift
@@ -7,12 +7,7 @@ struct SidebarView: View {
         List(selection: $eventFilter) {
             ForEach(EventFilter.allCases, id: \.rawValue) { filter in
                 NavigationLink(value: filter) {
-                    HStack {
-                        Image(systemName: filter.systemIcon)
-                            .foregroundColor(.accentColor)
-                            .frame(width: 18)
-                        Text(filter.rawValue)
-                    }
+                    Label(filter.rawValue, systemImage: filter.systemIcon)
                 }
             }
         }
